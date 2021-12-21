dfcec4d62bd0af7a99c25e0ac5d14d7211d39e8f
Dimmy Maenhout <dimmy.maenhout@icapps.com>
2021-12-21 17:14:34 +0100

    Added gpx file + mapbox navigation module config

--- TestMapboxNativeIOS/MapboxNavigationModuleConfig.swift
+++ TestMapboxNativeIOS/MapboxNavigationModuleConfig.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+class MapboxNavigationModuleConfig {
+    let accessToken: String
+    var locale: String
+    var enableLogging: Bool
+    
+    init(accessToken: String, locale: String, enableLogging: Bool) {
+        self.accessToken = accessToken
+        self.locale = locale
+        self.enableLogging = enableLogging
+    }
+}
